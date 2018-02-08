cd3b2bf070aadf26ec2510d1d8fbe06d8d02c1fe
Jake Baird <jake@launcherr.com>
2018-02-08 12:09:41 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700" rel="stylesheet">
+
+    </head>
+    <body data-smooth-scroll-offset="77">
+        <div class="nav-container"> </div>
+        <div class="main-container">
+            <section class="cover bg--secondary text-center unpad--bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="typed-headline"> <span class="h2 inline-block"><p><b>Keyline Realty </b>—</p></span> <span class="h2 inline-block typed-text typed-text--cursor color--primary" data-typed-strings="Lead Generator, Appraisals">ra</span> </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-10 col-md-offset-1"> <img class="unmarg--bottom box-shadow-wide" alt="Image" src="img/device-4.png">
+                            <div class="bg--dark box-shadow-wide"> <img class="unmarg--bottom" alt="Image" src="img/hero-1.jpg"> </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>
+
+    </body>
+
+</html>
